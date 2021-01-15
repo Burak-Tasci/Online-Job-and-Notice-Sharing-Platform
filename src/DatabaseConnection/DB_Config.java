3817c6773cb2260a5e6f5f1403c27e5e2ee707e2
Muhendiis <60064340+Muhendiis@users.noreply.github.com>
2021-01-16 00:57:19 +0300

    Add files via upload

---
+++ src/DatabaseConnection/DB_Config.java
@@ -0,0 +1,10 @@
+package DatabaseConnection;
+
+public class DB_Config {
+
+    //setting database connection configurations
+    public static String DatabaseName = "BURAKIDN";
+    public static String UserName = "";
+    public static String Password = "";
+
+}
